cb92cef7fc77113828c3da3912ff286f5f166c72
David Cheung <cheung.david@hotmail.ca>
2018-11-14 02:55:38 -0500

    debugging
    debugging

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -15,6 +15,7 @@
 #include <vfs.h>
 #include <vm.h>
 #include <test.h>
+#include <kern/fcntl.h>
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
 
@@ -260,7 +261,7 @@ int sys_execv(const char *program, char **args) {
     addrparams[i] = stackptr;
   } 
 
-  addrparams[argc] = NULL;
+  addrparams[argc] = 0;
 
   // Align character pointers
   while(stackptr % 4 != 0) {

0f658c2d4fb2fa77055cf6abd6e6ac000f459473
David Cheung <cheung.david@hotmail.ca>
2018-11-15 20:01:12 -0500

    dependency include
    dependency include

--- os161-1.99/kern/syscall/runprogram.c
+++ os161-1.99/kern/syscall/runprogram.c
@@ -44,7 +44,8 @@
 #include <vfs.h>
 #include <syscall.h>
 #include <test.h>
-
+#include <copyinout.h>
+ 
 /*
  * Load program "progname" and start running it in usermode.
  * Does not return except on error.

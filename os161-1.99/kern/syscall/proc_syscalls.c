4a050f98f582fa15b56a01463d0585a8b3bc61f8
David Cheung <cheung.david@hotmail.ca>
2018-11-16 00:06:35 -0500

    debugging execv
    debugging execv

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -196,7 +196,7 @@ int sys_execv(const char *program, char **args) {
     argc++;
   }
 
-  char **kernelargs = kmalloc(sizeof(char) * (argc + 1));
+  char **kernelargs = kmalloc(sizeof(char *) * (argc + 1));
 
   // Copy arguments into the kernel
   for(int i = 0; i < argc; i++) {
@@ -267,12 +267,7 @@ int sys_execv(const char *program, char **args) {
     addrparams[i] = stackptr;
   } 
 
-  addrparams[argc] = 0;
-
-  // Align character pointers
-  while(stackptr % 4 != 0) {
-    stackptr--;
-  }
+  addrparams[argc] = '\0';
 
   for(int j = argc; j >= 0; j--) {
     stackptr -= ROUNDUP(sizeof(vaddr_t), 4);
@@ -282,7 +277,7 @@ int sys_execv(const char *program, char **args) {
     }
   }
 
-  //as_destroy(as);
+  as_destroy(as);
 
   /* Warp to user mode. */
    enter_new_process(argc /*argc*/, (userptr_t) stackptr /*userspace addr of argv*/,
